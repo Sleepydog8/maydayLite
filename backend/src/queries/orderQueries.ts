9f5874ce5dfc258bc1760d9723770c9a0abb534c
Chol27 <tentchonlakorn@gmail.com>
2021-11-22 01:33:23 +0700

    getProductInOrder api

--- backend/src/queries/orderQueries.ts
+++ backend/src/queries/orderQueries.ts
@@ -1,4 +1,10 @@
 export const orderQueries = {
+  getProductInOrder: ({ CitizenID }) => {
+    return `SELECT P.*
+    FROM ordert O, product P
+    WHERE BuyerID = '${CitizenID}' AND O.orderID = P.orderID`
+  },
+
   checkout: ({ CitizenID }) => {
     return `UPDATE Ordert set OrderStatus = "P" WHERE BuyerID = citizenID AND OrderID = "1";
     DELETE FROM IN_CART I where I.CitizenID = '${CitizenID}'`
--- backend/src/routes/orders.ts
+++ backend/src/routes/orders.ts
@@ -3,6 +3,7 @@ import ctrl from '../controllers/OrderController'
 
 const routes = Router()
 
+routes.get('/:citizenID', ctrl.getProductInOrder)
 routes.put('/', ctrl.checkout)
 
 export default routes
