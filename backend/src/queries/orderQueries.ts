808dc395f40dcec18945e1a8f015f8d741e751eb
Pemtut <pemzar007@hotmail.com>
2021-11-22 01:47:17 +0700

    checkout

--- backend/src/queries/orderQueries.ts
+++ backend/src/queries/orderQueries.ts
@@ -1,6 +1,6 @@
 export const orderQueries = {
   checkout: ({ CitizenID }) => {
-    return `UPDATE Ordert set OrderStatus = "P" WHERE BuyerID = citizenID AND OrderID = "1";
+    return `UPDATE Ordert set OrderStatus = "P" WHERE BuyerID = '${CitizenID}' AND Orderstatus = 'I'
     DELETE FROM IN_CART I where I.CitizenID = '${CitizenID}'`
   },
 }
