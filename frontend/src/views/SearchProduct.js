b802e73d7cdf491bbc60120929fa4467079d9a53
Jiratchaya Puvapaisankit <jinnie0728@gmail.com>
2021-11-22 02:50:59 +0700

    Merge

--- frontend/src/views/SearchProduct.js
+++ frontend/src/views/SearchProduct.js
@@ -1,71 +1,29 @@
-<<<<<<< HEAD
 import { useEffect, useState } from 'react'
 import * as api from '../lib/api'
+import heart from './image/heart.png'
+import shopping_cart from './image/shopping-cart.png'
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content')
+    var i
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show')
+      }
+    }
+  }
+}
 
 function SearchProduct() {
   const [products, setProducts] = useState([])
-||||||| 399839a
-import { useEffect } from 'react'
-
-var p = [
-  {
-    ProductId: 1,
-    Name: 'Lady Dior',
-    Price: 20000,
-    Catagory: 'Shirt',
-  },
-  {
-    ProductId: 2,
-    Name: 'LV หลุยส์แท้ 10%',
-    Price: 150000,
-    Category: 'Shirt',
-  },
-  {
-    ProductId: 3,
-    Name: 'Tiew Handsome',
-    Price: 200000,
-    Category: 'Shoes',
-  },
-]
-
-function search(catagory) {
-  alert(catagory)
-}
-=======
-import { useEffect } from 'react'
-import heart from './image/heart.png'
-import shopping_cart from './image/shopping-cart.png'
-var p = [
-  {
-    ProductId: 1,
-    Name: 'Lady Dior',
-    Price: 20000,
-    Catagory: 'Shirt',
-  },
-  {
-    ProductId: 2,
-    Name: 'LV หลุยส์แท้ 10%',
-    Price: 150000,
-    Category: 'Shirt',
-  },
-  {
-    ProductId: 3,
-    Name: 'Tiew Handsome',
-    Price: 200000,
-    Category: 'Shoes',
-  },
-]
-
-function search(catagory) {
-  alert(catagory)
-}
->>>>>>> 7bc6100e9e3b910f98a97fcd479a5878674c46a6
 
   useEffect(() => {
     search('ALL')
   }, [])
 
-<<<<<<< HEAD
   async function search(catagory) {
     try {
       const data = await api.product.get(catagory)
@@ -88,60 +46,8 @@ function search(catagory) {
       const data = await api.cart.add(ProductID)
     } catch (error) {
       console.log('add to cart Error')
-||||||| 399839a
-async function showItemsInProductList() {
-  var table_body = document.getElementById('search-productlist')
-  console.log('table_body')
-  table_body.innerHTML = ''
-  var items = p
-  items.map((item) => {
-    table_body.innerHTML += `
-            <tr id="${item.ProductId}">
-                <td>${item.ProductId}</td>
-                <td>${item.Name}</td>
-                <td>${item.Price}</td>
-                <td><button onclick="addToWishlist('${item.ProductId}')"><img src="image/heart.png" width="20" height="20" ></button></td>
-                <td><button onclick="alert('${item.ProductId}')"><img src="image/shopping-cart.png" width="20" height="20"></button></td>
-            </tr>
-            `
-  })
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content')
-    var i
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
-      }
-=======
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content')
-    var i
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
-      }
->>>>>>> 7bc6100e9e3b910f98a97fcd479a5878674c46a6
     }
   }
-
-<<<<<<< HEAD
-||||||| 399839a
-function SearchProduct() {
-  useEffect(() => {
-    showItemsInProductList()
-  }, [])
-=======
-function SearchProduct() {
-  useEffect(() => {}, [])
->>>>>>> 7bc6100e9e3b910f98a97fcd479a5878674c46a6
   return (
     <div>
       <h1
@@ -175,57 +81,6 @@ function SearchProduct() {
           <option value="Shoes">Shoes</option>
         </select>
       </div>
-<<<<<<< HEAD
-      <table>
-        <thead>
-          <tr>
-            <th>Product Id</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Wishlist</th>
-            <th>Cart</th>
-          </tr>
-          {products.map((item) => (
-            <tr id={item.ProductId}>
-              <td>{item.ProductId}</td>
-              <td>{item.Name}</td>
-              <td>{item.Price}</td>
-              <td>
-                <button onClick={() => addToWishlist(item.ProductId)}>
-                  <img src="image/heart.png" width="20" height="20" />
-                </button>
-              </td>
-              <td>
-                <button onClick={() => addToInCart(item.ProductId)}>
-                  <img src="image/shopping-cart.png" width="20" height="20" />
-                </button>
-              </td>
-||||||| 399839a
-      <table>
-        <thead>
-          <tr>
-            <th>Product Id</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Wishlist</th>
-            <th>Cart</th>
-          </tr>
-          {p.map((item) => (
-            <tr id={item.ProductId}>
-              <td>{item.ProductId}</td>
-              <td>{item.Name}</td>
-              <td>{item.Price}</td>
-              <td>
-                <button onClick={() => addToWishlist(item.ProductId)}>
-                  <img src="image/heart.png" width="20" height="20" />
-                </button>
-              </td>
-              <td>
-                <button onClick={() => addToInCart(item.ProductId)}>
-                  <img src="image/shopping-cart.png" width="20" height="20" />
-                </button>
-              </td>
-=======
       <div
         style={{
           justifyContent: 'center',
@@ -249,9 +104,8 @@ function SearchProduct() {
               <th>Price</th>
               <th>Wishlist</th>
               <th>Cart</th>
->>>>>>> 7bc6100e9e3b910f98a97fcd479a5878674c46a6
             </tr>
-            {p.map((item) => (
+            {products.map((item) => (
               <tr id={item.ProductId}>
                 <td>{item.ProductId}</td>
                 <td>{item.Name}</td>
